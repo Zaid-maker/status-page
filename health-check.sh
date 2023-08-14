310ac577a0644f262afae1a3ca0b9f1eca33b166
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-08-15 01:47:33 +0500

    Using my bot acc now

--- health-check.sh
+++ health-check.sh
@@ -56,8 +56,8 @@ done
 if [[ $commit == true ]]
 then
   # Let's make Vijaye the most productive person on GitHub.
-  git config --global user.name 'Zaid-maker'
-  git config --global user.email 'pzhafeez@gmail.com'
+  git config --global user.name 'devmirza-bot'
+  git config --global user.email 'mirzazaid4740@gmail.com'
   git add -A --force logs/
   git commit -am '[Automated] Update Health Check Logs'
   git push
