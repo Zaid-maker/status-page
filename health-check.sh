8dda34b8bb2d1530f37b41cd187deb4f2d68e4b9
DevMirza <pzhafeez@gmail.com>
2024-06-03 13:16:16 +0000

    Revert "Update health-check.sh"
    This reverts commit 18c651d1a35ee9c60c84b94a8380df24372f84cc.

--- health-check.sh
+++ health-check.sh
@@ -1,20 +1,21 @@
 commit=true
 origin=$(git remote get-url origin)
-
-# Check if the script is running on the "Zaid-maker/status-page" repository
-if [[ $origin == *Zaid-maker/status-page* ]]; then
+if [[ $origin == *Zaid-maker/status-page* ]]
+then
   commit=true
 fi
 
 KEYSARRAY=()
 URLSARRAY=()
 
-# Read URLs and keys from the configuration file
 urlsConfig="./urls.cfg"
 echo "Reading $urlsConfig"
-while IFS='=' read -r key url; do
-  KEYSARRAY+=("$key")
-  URLSARRAY+=("$url")
+while read -r line
+do
+  echo "  $line"
+  IFS='=' read -ra TOKENS <<< "$line"
+  KEYSARRAY+=(${TOKENS[0]})
+  URLSARRAY+=(${TOKENS[1]})
 done < "$urlsConfig"
 
 echo "***********************"
@@ -22,38 +23,42 @@ echo "Starting health checks with ${#KEYSARRAY[@]} configs:"
 
 mkdir -p logs
 
-for ((index=0; index < ${#KEYSARRAY[@]}; index++)); do
+for (( index=0; index < ${#KEYSARRAY[@]}; index++))
+do
   key="${KEYSARRAY[index]}"
   url="${URLSARRAY[index]}"
   echo "  $key=$url"
 
-  # Retry up to 4 times with 5 seconds delay between attempts
-  for i in {1..4}; do
-    response=$(curl --write-out '%{http_code}' --silent --output /dev/null "$url")
-    if [[ "$response" -eq 200 || "$response" -eq 202 || "$response" -eq 301 || "$response" -eq 307 ]]; then
+  for i in 1 2 3 4; 
+  do
+    response=$(curl --write-out '%{http_code}' --silent --output /dev/null $url)
+    if [ "$response" -eq 200 ] || [ "$response" -eq 202 ] || [ "$response" -eq 301 ] || [ "$response" -eq 307 ]; then
       result="success"
-      break
     else
       result="failed"
     fi
+    if [ "$result" = "success" ]; then
+      break
+    fi
     sleep 5
   done
-
   dateTime=$(date +'%Y-%m-%d %H:%M')
-  if [[ $commit == true ]]; then
-    echo "$dateTime, $result" >> "logs/${key}_report.log"
+  if [[ $commit == true ]]
+  then
+    echo $dateTime, $result >> "logs/${key}_report.log"
+    # By default we keep 4000 last log entries.  Feel free to modify this to meet your needs.
+    echo "$(tail -6000 logs/${key}_report.log)" > "logs/${key}_report.log"
   else
-    echo "  $dateTime, $result"
+    echo "    $dateTime, $result"
   fi
 done
 
-if [[ $commit == true ]]; then
-  # Set Git user information (modify if needed)
-  git config --global user.name "Zaid-maker"
-  git config --global user.email "pzhafeez@gmail.com"
-
-  # Add and commit changes to the logs directory
+if [[ $commit == true ]]
+then
+  # Let's make Vijaye the most productive person on GitHub.
+  git config --global user.name 'Zaid-maker'
+  git config --global user.email 'pzhafeez@gmail.com'
   git add -A --force logs/
-  git commit -am "[Automated] Update Health Check Logs"
+  git commit -am '[Automated] Update Health Check Logs'
   git push
 fi
