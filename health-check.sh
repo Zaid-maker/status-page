f7d5990f8f2975c9888267dc260615f4c40a27d4
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-11-06 18:59:44 +0500

    Update health-check.sh

--- health-check.sh
+++ health-check.sh
@@ -47,7 +47,7 @@ do
   then
     echo $dateTime, $result >> "logs/${key}_report.log"
     # By default we keep 4000 last log entries.  Feel free to modify this to meet your needs.
-    echo "$(tail -10000 logs/${key}_report.log)" > "logs/${key}_report.log"
+    echo "$(tail -12000 logs/${key}_report.log)" > "logs/${key}_report.log"
   else
     echo "    $dateTime, $result"
   fi
